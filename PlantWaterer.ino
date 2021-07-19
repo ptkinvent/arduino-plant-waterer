8e82d316f815c87eb845935b6c9b39b23659675e
Prateek Sahay <ptkinvent@gmail.com>
2021-07-19 10:41:59 -0700

    Added motor class

--- PlantWaterer.ino
+++ PlantWaterer.ino
@@ -4,25 +4,25 @@
  * Attach soil capacitive sensor to A0.
  */
 
-// the setup function runs once when you press reset or power the board
-void setup() {
-  //Setup Channel A
-  pinMode(12, OUTPUT); //Initiates Motor Channel A pin
-  pinMode(9, OUTPUT); //Initiates Brake Channel A pin
+#include "Motor.h"
 
+MotorA motor;
+
+
+void setup()
+{
   Serial.begin(9600);
 }
 
-// the loop function runs over and over again forever
-void loop() {
-  // forward @ full speed
-  digitalWrite(12, HIGH); //Establishes forward direction of Channel A
-  digitalWrite(9, LOW);   //Disengage the Brake for Channel A
-  analogWrite(3, 0);   //Spins the motor on Channel A at full speed
+
+
+void loop()
+{
+  motor.spinForward(255);
 
   delay(1500);
   
-  digitalWrite(9, HIGH); //Eengage the Brake for Channel A
+  motor.brake();
 
   delay(3000);
 

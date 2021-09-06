3465c6125540c27d3e12cf4f2793a5e5f0e7ef56
Prateek Sahay <ptkinvent@gmail.com>
2021-09-05 19:17:19 -0700

    Added LCD display of moisture level

--- PlantWaterer.ino
+++ PlantWaterer.ino
@@ -4,9 +4,12 @@
  * Attach soil capacitive sensor to A5.
  */
 
+#include <LiquidCrystal.h>
 #include "Motor.h"
 #include "MoistureSensor.h"
 
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 MotorA motor;
 MoistureSensor moistureSensor(A5);
 
@@ -14,17 +17,29 @@ MoistureSensor moistureSensor(A5);
 void setup()
 {
     Serial.begin(9600);
+    lcd.begin(16, 2);
 }
 
 
 
 void loop()
 {
-    // Desired moisture level is 0.32
-    float val = moistureSensor.sense();
-    Serial.println(val);
+    // Desired moisture level is 0.31
+    float desiredVal = 0.31;
+    float currentVal = moistureSensor.sense();
+    Serial.println(currentVal);
+    lcd.setCursor(0, 0);
+    lcd.print("Current: ");
+    lcd.print(100 * (1 - currentVal));
+    lcd.print("%");
+    lcd.setCursor(0, 1);
+    lcd.print("Desired: ");
+    lcd.print(100 * (1 - desiredVal));
+    lcd.print("%");
+    motor.brake();
 
-    if (val > 0.32)
+    delay(500);
+    if (currentVal < desiredVal)
     {
         motor.spinForward(255);
         delay(1500);

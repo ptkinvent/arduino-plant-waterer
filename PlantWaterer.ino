d2ca42216553bf6f1b0c65aed3d3d41de2468215
Prateek Sahay <ptkinvent@gmail.com>
2021-12-20 21:22:01 -0800

    WIP: Removed LCD code, adding interrupt

--- PlantWaterer.ino
+++ PlantWaterer.ino
@@ -8,8 +8,6 @@
 #include "Motor.h"
 #include "MoistureSensor.h"
 
-const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
-LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 MotorA motor;
 MoistureSensor moistureSensor(A5);
 
@@ -17,7 +15,6 @@ MoistureSensor moistureSensor(A5);
 void setup()
 {
     Serial.begin(9600);
-    lcd.begin(16, 2);
 }
 
 
@@ -27,23 +24,25 @@ void loop()
     // Desired moisture level is 0.31
     float desiredVal = 0.31;
     float currentVal = moistureSensor.sense();
-    Serial.println(currentVal);
-    lcd.setCursor(0, 0);
-    lcd.print("Current: ");
-    lcd.print(100 * (1 - currentVal));
-    lcd.print("%");
-    lcd.setCursor(0, 1);
-    lcd.print("Desired: ");
-    lcd.print(100 * (1 - desiredVal));
-    lcd.print("%");
+    Serial.print("Current: ");
+    Serial.print((1 - currentVal) * 100);
+    Serial.print(" | ");
+    Serial.print("Desired: ");
+    Serial.print((1 - desiredVal) * 100);
+    Serial.print(" | ");
     motor.brake();
 
     delay(500);
-    if (currentVal < desiredVal)
+    if (currentVal > desiredVal)
     {
-        motor.spinForward(255);
-        delay(1500);
-        motor.brake();
-        delay(10000);
+        Serial.println("Pumping...");
+//        motor.spinForward(255);
+//        delay(1500);
+//        motor.brake();
+//        delay(10000);
+    }
+    else
+    {
+        Serial.println("No pump");
     }
 }
